7a99e6de7d2c1c6a0572bad926458b6b5d24efd7
Yurip3 <yuripedro1@gmail.com>
2016-07-21 21:10:48 -0300

    WATTino Software V1.2
    Leitura do WATTino pela Serial

---
+++ WATTino_com_Millis/WATTino_com_Millis.ino
@@ -0,0 +1,43 @@
+const int sensorT = A0;  //Sensor de Tensao
+const int sensorC = A1; //Sensor de Corrente
+
+float Tensao = 0;              // Variavel que guarda os Valores convertidos de Tensao
+float Corrente = 0;            // Variavel que guarda os valores convertidos de Corrente
+float LeituraTensao = 0;       // Variavel que guarda os valores lidos de Tensao
+float LeituraCorrente = 0;     // Variavel que guarda os valores lidos de Corrente
+
+
+unsigned long  tempoAnterior = 0;
+const long intervalo = 67;
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode( sensorT, INPUT);              //  Inicia os Sensores
+  pinMode( sensorC, INPUT);             //      como Entrada
+}
+
+void loop() 
+{
+ unsigned long tempoAtual = millis();
+
+
+
+ if ( tempoAtual - tempoAnterior >= intervalo)
+{
+  LeituraTensao = analogRead(sensorT);                                    // Faz a Leitura do Sensor de Tensao
+  LeituraCorrente = analogRead(sensorC);                                  // Faz a Leitura do Sensor de Corrente
+  Tensao = (((LeituraTensao - 511.5) * 1207096)/1757369.88);              // Converte os valores lidos para Tensao
+  Corrente = (((LeituraCorrente - 511.5) * 5000)/511500);                 // Converte os valores lidos para Corrente                 
+  Serial.print(Tensao);  
+  Serial.print(" "); 
+  Serial.println(Corrente); 
+
+  tempoAnterior = tempoAtual;
+}
+
+
+             
+                   
+}
